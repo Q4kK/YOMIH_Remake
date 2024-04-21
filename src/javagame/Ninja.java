b7bbb8e0a9f39f55bfd021d510407759513c867e
Q4kK <125313163+Q4kK@users.noreply.github.com>
2024-04-20 23:18:32 -0500

    Feat: removed spritesheets & redid `Ninja` Class

--- src/javagame/Ninja.java
+++ src/javagame/Ninja.java
@@ -1,19 +1,19 @@
 package javagame;
 
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
 public class Ninja {
 
-	private float speed;
 	private int health;
-	private boolean active;
+	private boolean active = true;
+	private SpriteSheet sheet = null;
+	private Image image = null;
 
-	public Ninja(float speed, int health, boolean active) {
-		this.speed = speed;
+	public Ninja(int health, String image) throws SlickException {
 		this.health = health;
-		this.active = active;
-	}
-
-	public void setSpeed(float speed) {
-		this.speed = speed;
+		this.image = new Image(image);
 	}
 
 	public void setHealth(int health) {
@@ -24,15 +24,35 @@ public class Ninja {
 		this.active = active;
 	}
 
-	public float getSpeed() {
-		return this.speed;
-	}
-
 	public int getHealth() {
 		return this.health;
 	}
 
 	public boolean isActive() {
 		return this.active;
+	}
+
+	public void setSheet(SpriteSheet sheet) {
+		this.sheet = sheet;
+	}
+
+	public SpriteSheet getSheet() {
+		return this.sheet;
+	}
+
+	public void setImage(String image) throws SlickException {
+		this.image = new Image(image);
+	}
+
+	public Image getImage() {
+		return this.image;
+	}
+
+	public Image getImage(int x, int y) throws SlickException {
+		return new SpriteSheet(this.image, 128, 128).getSubImage(x, y);
+	}
+
+	public Image getImage(String image, int x, int y) throws SlickException {
+		return new SpriteSheet(image, 128, 128).getSubImage(x, y);
 	}
 }
